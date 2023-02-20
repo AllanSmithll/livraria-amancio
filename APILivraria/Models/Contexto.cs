1c4f2e21bfcff4df3009f2c263e2e98296e363ab
allansmithll <allanammancio2004@gmail.com>
2023-02-20 17:49:40 -0300

    Context

---
+++ APILivraria/Models/Contexto.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace APILivraria.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Livro> Livros { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+            
+        } 
+    }
+}
--- APILivraria/Models/Livro.cs
+++ APILivraria/Models/Livro.cs
@@ -1,14 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace APILivraria.Models
 {
     public class Livro
     {
-        public static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+        public Livro(string isbn, string titulo, string subtitulo, string resumo, string genero, int quantPaginas, DateOnly publicacao, int edicao, string nomeEditora, string nomeAutor)
+        {
+            Isbn = isbn;
+            Titulo = titulo;
+            Subtitulo = subtitulo;
+            Resumo = resumo;
+            Genero = genero;
+            QuantPaginas = quantPaginas;
+            Publicacao = publicacao;
+            Edicao = edicao;
+            this.nomeEditora = nomeEditora;
+            this.nomeAutor = nomeAutor;
         }
+
+        public string Isbn { get; set; }
+
+        public string Titulo { get; set;}
+
+        public string  Subtitulo { get; set; }
+
+        public string Resumo { get; set; }
+
+        public string Genero { get; set; }
+
+        public int QuantPaginas { get; set; }
+
+        public DateOnly Publicacao { get; set; }
+
+        public int Edicao { get; set; }
+
+        public string nomeEditora { get; set; }
+
+        public string nomeAutor { get; set; }
     }
 }
