92fb8d35bed9bb811a79c8a00b37f521eaf9c698
allansmithll <allanammancio2004@gmail.com>
2023-02-19 22:59:55 -0300

    Create Class: Livro created

---
+++ APILivraria/Models/Livro.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APILivraria.Models
+{
+    public class Livro
+    {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
