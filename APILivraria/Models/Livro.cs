60cf818415bcb1c3b6fabaa24903d477831a3b40
allansmithll <allanammancio2004@gmail.com>
2023-02-21 17:07:55 -0300

    Backend no recomeço

---
+++ APILivraria/Models/Livro.cs
@@ -0,0 +1,21 @@
+namespace APILivraria.Models.Livro
+{
+    public class Pessoa
+    {
+        public string Isbn { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Subtitulo { get; set; }
+
+        public string Resumo { get; set; }
+
+        public string Genero { get; set; }
+
+        public int QuantPaginas { get; set; }
+
+        public DateOnly DataPublicacao{ get; set; }
+
+        public 
+    }
+}
